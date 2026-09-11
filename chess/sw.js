cbb35c81238d96137833a2d3d13d218d4d96781f
js.park <coolgary825@gmail.com>
2026-09-11 20:35:54 +0900

    fix: link chess home button to the published arcade

--- chess/sw.js
+++ chess/sw.js
@@ -1,4 +1,4 @@
-const CACHE='junwoo-chess-v2';
+const CACHE='junwoo-chess-v3';
 const FILES=['./','index.html','style.css','app.js','domain.js','engine.js','puzzles.js','board3d.js','manifest.json','icon.svg','icon-192.png','icon-512.png','THIRD_PARTY.md','vendor/chess.js','vendor/chess.LICENSE','vendor/stockfish-18-lite-single.js','vendor/stockfish-18-lite-single.wasm','vendor/stockfish.COPYING.txt','vendor/three/three.module.js','vendor/three/three.core.js','vendor/three/OrbitControls.js','vendor/three/LICENSE',...['w','b'].flatMap(c=>['K','Q','R','B','N','P'].map(t=>`pieces/${c}${t}.svg`))];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('junwoo-chess-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
